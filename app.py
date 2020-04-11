621a41d78490dc0d108d61d5e7a41a4f5df16453
Seanán Ó Coistín <seananoc@gmail.com>
2020-04-11 07:02:20 +0000

    I added various new files and code to the files

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'acmebd'
+app.config["MONGO_URI"] = 'mongodb+srv://stiurthoir:b1o2l3l4i5x6@cluster0-gaug0.mongodb.net/acmeBD?retryWrites=true&w=majority'
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Acme Business Directory'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
