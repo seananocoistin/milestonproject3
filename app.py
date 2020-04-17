363f1321fb840f3e2699b654b9315f6cb9849790
Seanán Ó Coistín <seananoc@gmail.com>
2020-04-17 20:42:29 +0000

    A shit of load of new stuff and files

--- app.py
+++ app.py
@@ -5,15 +5,33 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'acmebd'
-app.config["MONGO_URI"] = 'mongodb+srv://stiurthoir:b1o2l3l4i5x6@cluster0-gaug0.mongodb.net/acmeBD?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://stiurthoir:b1o2l3l4i5x6@cluster0-gaug0.mongodb.net/acmebd?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
-@app.route('/')
-@app.route('/get_tasks')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/tasks")
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+@app.route("/add_task")
+def add_task():
+    return render_template("addtask.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
