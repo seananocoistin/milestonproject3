d654c37d0214089b8d681c3234269b520eca43a9
Seanán Ó Coistín <seananoc@gmail.com>
2020-08-24 16:59:20 +0000

    Added new lines to the app.py to make the records of new users visible and editable

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
+import json
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+author = ""
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'acmebd'
 app.config["MONGO_URI"] = 'mongodb+srv://stiurthoir:b1o2l3l4i5x6@cluster0-gaug0.mongodb.net/acmebd?retryWrites=true&w=majority'
@@ -25,13 +27,34 @@ def signup():
 def contact():
     return render_template("contact.html")
 
+
 @app.route("/tasks")
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", tasks=mongo.db.users.find())
 
-@app.route("/addtask")
-def add_task():
-    return render_template("addtask.html")
+@app.route("/ownrecords")
+def get_ownrecords():
+    return render_template("tasks.html", tasks=mongo.db.users.find({"author":"Seanán"}))
+
+@app.route("/create")
+def add_create():
+    return render_template("create.html")
+
+@app.route("/addrecord", methods=["POST"])
+def addrecord():
+    if request.method == "POST":
+        newrecord = request.form.to_dict()
+        newrecord.update({"author":author})
+        mongo.db.users.insert(newrecord)
+        return get_tasks()
+
+@app.route("/register", methods=["POST"])
+def register():
+    if request.method == "POST":
+        user=mongo.db.accounts.insert(request.form.to_dict())
+        global author
+        author=request.form["username"]
+        return add_create()
 
 @app.route("/edit")
 def add_edit():
